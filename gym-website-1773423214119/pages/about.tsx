ed65f99c387241be9ac41a50091c2a0682fcff42
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 23:03:39 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773423214119/pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About Us</title>
+      </Head>
+      <h1>About Us</h1>
+      <p>We are a team of fitness enthusiasts dedicated to helping you achieve your fitness goals.</p>
+    </div>
+  );
+};
+
+export default About;
